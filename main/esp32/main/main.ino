78f1ea4b33919543b7ca0655d983f00e5e9868f1
MataiMoorfield <matai@moorfield.co.nz>
2024-03-23 07:44:42 +1300

    Now hosts random JSON data

--- main/esp32/main/main.ino
+++ main/esp32/main/main.ino
@@ -1,69 +1,62 @@
 #include <WiFi.h>
 #include <WebServer.h>
 #include <ArduinoJson.h>
-#include <Wire.h>
-#include <MPU6050.h>
 
-const char *ssid = "ORBI37";
-const char *password = "helpfulshrub878";
+const char *ssid = "network_name";          // Change with network name
+const char *password = "network_password";  // Change with network password
 
 WebServer server(80);
 
-MPU6050 mpu;
-
 void handleRoot() {
   StaticJsonDocument<200> doc;
-  
-  float ax = mpu.getAccelerationX();
-  float ay = mpu.getAccelerationY();
-  float az = mpu.getAccelerationZ();
-  float gx = mpu.getRotationX();
-  float gy = mpu.getRotationY();
-  float gz = mpu.getRotationZ();
 
-  Serial.print(ax);
-  Serial.print(',');
-  Serial.print(ay);
-  Serial.print(',');
-  Serial.print(az);
-  Serial.print(','); 
-  Serial.print(gx);
-  Serial.print(','); 
-  Serial.print(gy);
-  Serial.print(','); 
-  Serial.println(gz); 
-
-  doc["acceleration_x"] = ax;
-  doc["acceleration_y"] = ay;
-  doc["acceleration_z"] = az;
-  doc["gyroscope_x"] = gx;
-  doc["gyroscope_y"] = gy;
-  doc["gyroscope_z"] = gz;
+  String words[] = {"apple", "banana", "orange", "grape", "kiwi"};
+  int numWords = sizeof(words) / sizeof(words[0]);
   
+  int randomIndex = random(0, numWords);
+  
+  float randomValue = random(0, 100); 
+  
+  String randomWord = words[randomIndex];
+  
+  Serial.print("Random Word: ");
+  Serial.print(randomWord);
+  Serial.print(", Random Value: ");
+  Serial.println(randomValue); 
+
+  doc["random_word"] = randomWord;
+  doc["random_value"] = randomValue;
+
   String json;
   serializeJson(doc, json);
   
   server.send(200, "application/json", json);
 }
 
-
+void printWiFiInfo() {
+  Serial.println("");
+  Serial.println("WiFi connected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
 
 void setup() {
   Serial.begin(115200);
-
-  Wire.begin();
-  mpu.initialize();
   
+  randomSeed(analogRead(0));
+
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi..");
   }
-  Serial.println("Connected to WiFi");
+  
+  printWiFiInfo();
 
   server.on("/", handleRoot);
-
+  
   server.begin();
+  
   Serial.println("HTTP server started");
 }
 
